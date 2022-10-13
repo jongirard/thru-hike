f9770d92fc77fa6194452253e0c79fdcdee94a89
Jamie Phelps <jamie@jamiephelps.com>
2022-10-13 13:42:36 -0700

    Plot data on the map
    Followed this guide: https://visgl.github.io/react-map-gl/docs/get-started/adding-custom-data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import './App.css';
-import Map from 'react-map-gl';
+import Map, { Source, Layer } from 'react-map-gl';
+
+import data from './data'
 
 function App() {
+
+	const layerStyle = {
+		id: 'point',
+		type: 'circle',
+		paint: {
+			'circle-radius': 10,
+			'circle-color': '#007cbf'
+		}
+	};
+
+	const center = data.features.at(Math.floor(data.features.length/2)).geometry.coordinates
+
   return (
     <div className="App">
       <Map
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 14
+          longitude: center[0],
+          latitude: center[1],
+					zoom: 8
         }}
         style={styles.viewPort}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
+      >
+		<Source id="trailPoints" type="geojson" data={data}>
+			<Layer {...layerStyle} />
+		</Source>
+		</Map>
     </div>
   );
 }
